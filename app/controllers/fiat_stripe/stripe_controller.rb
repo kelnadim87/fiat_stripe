3f8834d092bb4550fc6788a69f0f6d84151bf5d4
Andrew Haines <andrew@fiatinsight.com>
2019-01-12 08:12:50 -0500

    Added basic one-time payments to controller

--- app/controllers/fiat_stripe/stripe_controller.rb
+++ app/controllers/fiat_stripe/stripe_controller.rb
@@ -2,19 +2,35 @@ module FiatStripe
   class StripeController < ActionController::Base
 
     def create_stripe_customer_id
-      # TODO: Figure out how to handle this
       object_class = params[:object_class].constantize
       object_id = params[:object_id].to_i
 
-      customer = Stripe::Customer.create(
-        { description: params[:name],
-          email: params[:email]
+      customer = Stripe::Customer.create({
+        description: params[:name],
+        email: params[:email]
         },
         api_key: object_class.find(object_id).stripe_api_key
       )
 
       object_class.find(object_id).update_attributes(stripe_customer_id: customer.id)
       redirect_back(fallback_location: nil, notice: 'Your payment profile was created.')
+    end
+
+    def one_time_payment
+      object_class = params[:object_class].constantize
+      object_id = params[:object_id].to_i
+
+      charge = Stripe::Charge.create({
+        customer: params[:customer_id], # Ideally, this would be further encrypted somehow
+        amount: 999,
+        currency: 'usd',
+        description: 'Example charge',
+        # source: token,
+        },
+        api_key: object_class.find(object_id).stripe_api_key
+      )
+
+      redirect_back(fallback_location: nil, notice: 'Your payment was created!')
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 FiatStripe::Engine.routes.draw do
   resources :stripe do
     post :create_stripe_customer_id, on: :collection
+    post :one_time_payment, on: :collection
   end
 end
